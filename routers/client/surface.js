2aee016b156a992966481dc6e0973c428079b119
gahramanova <narminhikmat@gmail.com>
2025-04-24 16:12:28 +0200

    Added all get requests to surface

--- routers/client/surface.js
+++ routers/client/surface.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const { productListAll, productListById, productByPaginationList } = require("../../controllers/product/product")
 const { categoryList, categoryListById } = require("../../controllers/product/category")
+const { aboutAllList, howWeCanHelpAll } = require("../../controllers/components/about")
+const { generalInformationList, generalInformationSingleList } = require("../../controllers/ubwo/generalinformation")
+const { homeAllList } = require("../../controllers/components/home")
+const { teamAllList } = require("../../controllers/components/team")
 const router = express.Router()
 
 
@@ -10,6 +14,20 @@ router.get("/productpag", productByPaginationList)
 
 router.get('/category',categoryList);
 router.get('/category/:id',categoryListById);
+
+router.get("/", aboutAllList)
+router.get("/howWeCanHelp", howWeCanHelpAll)
+
+router.get('/', generalInformationList);
+router.get("/:id", generalInformationSingleList);
+
+router.get("/", homeAllList)
+
+router.get("/", teamAllList)
+
+
+
+
 
 
 module.exports = router
