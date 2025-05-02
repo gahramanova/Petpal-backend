c5d8e71ea8b71ba069d4b6f05d0189c776371c02
gahramanova <narminhikmat@gmail.com>
2025-05-02 16:23:40 +0200

    Update in middleware

--- middlewares/accesstoken.js
+++ middlewares/accesstoken.js
@@ -3,14 +3,18 @@ const accesstoken = async (req, res, next) => {
       const token = "f20e26bb783ea89ef58823b877337a2e";
       const incomingToken = req.headers["petpal-token"];
   
-      if (!incomingToken || incomingToken !== token) {
+      if (!incomingToken) {
         return res.status(401).send("no-access");
+      }
+  
+      if (incomingToken !== token) {
+        return res.status(401).send("invalid token");
       }
   
       next();
     } catch (error) {
-      console.error(error);
-      res.status(500).send("server-error");
+      console.log(error);
+      res.status(500).send("server error");
     }
   };
   
